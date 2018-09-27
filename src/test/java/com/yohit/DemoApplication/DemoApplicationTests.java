8382623ff49bf67c24d45c338812f3f9e63983b1
Yohit Nalajala <yohit.n@gmail.com>
2018-09-27 03:12:02 -0400

    Cleanup unused imports

--- src/test/java/com/yohit/DemoApplication/DemoApplicationTests.java
+++ src/test/java/com/yohit/DemoApplication/DemoApplicationTests.java
@@ -7,12 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
