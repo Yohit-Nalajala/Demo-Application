fdfaeab43db0d93ddd11649177d410fb6bddab0a
Yohit Nalajala <yohit.n@gmail.com>
2018-09-27 03:09:08 -0400

    Added Unit tests and Jacoco test report plugin

---
+++ src/test/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO STATION VALUES ( 'station-id-test', 'call-sign-test', 'true', 'test');
+INSERT INTO STATION VALUES ( 'station-id-test-1', 'call-sign-test-1', 'true', 'test-1');
+INSERT INTO STATION VALUES ( 'station-id-test-2', 'call-sign-test-2', 'true', 'test-2');
+INSERT INTO STATION VALUES ( 'station-id-test-3', 'call-sign-test-3', 'true', 'test-3');
+INSERT INTO STATION VALUES ( 'station-id-test-4', 'call-sign-test-4', 'true', 'test-4');
+INSERT INTO STATION VALUES ( 'station-id-test-5', 'call-sign-test-5', 'true', 'test-5');
+INSERT INTO STATION VALUES ( 'station-id-test-6', 'call-sign-test-6', 'true', 'test-5');
+INSERT INTO STATION VALUES ( 'station-id-test-7', 'call-sign-test-7', 'true', 'test-6');
+INSERT INTO STATION VALUES ( 'station-id-test-8', 'call-sign-test-8', 'true', 'test-7');
+INSERT INTO STATION VALUES ( 'station-id-test-9', 'call-sign-test-9', 'true', 'test-8');
